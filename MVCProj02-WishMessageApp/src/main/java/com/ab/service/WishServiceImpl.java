337b725e5782aa1a098339975f7a90771ce57168
Abinash Biswal <abinashbiswal058@gmail.com>
2020-01-26 19:44:36 +0530

    [CPOP-1262020]- made changes in result String in WishMEssageImpl

--- MVCProj02-WishMessageApp/src/main/java/com/ab/service/WishServiceImpl.java
+++ MVCProj02-WishMessageApp/src/main/java/com/ab/service/WishServiceImpl.java
@@ -8,7 +8,7 @@ public class WishServiceImpl implements WishService {
 	@Override
 	public String generateWishMessage() {
 		
-		return "GoodMorning Always.. :)";
+		return "GoodNight Always.. :)";
 	}
 
 }
